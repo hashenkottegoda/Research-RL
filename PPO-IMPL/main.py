5622a0b1e2fd5860e418acfdfc1c609059f2c876
Hashen Kottegoda <thkottegoda@gmail.com>
2023-04-16 01:02:48 +0530

    loading saved models, removed episodic behavur

--- PPO-IMPL/main.py
+++ PPO-IMPL/main.py
@@ -110,7 +110,7 @@ if __name__ == '__main__':
     agent = Agent(n_actions=len(env.action_space), batch_size=batch_size, 
                     alpha=alpha, n_epochs=n_epochs, 
                     input_dims=env.sample_observation.shape)
-    n_games = 100
+    # n_games = 100
 
     figure_file = 'plots/cartpole.png'
 
@@ -122,29 +122,35 @@ if __name__ == '__main__':
     avg_score = 0
     n_steps = 0
 
-    for i in range(n_games):
-        observation = env.reset()
-        done = False
-        score = 0
-        while not done:
-            action, prob, val = agent.choose_action(observation)
-            real_action = env.action_space[action]
-            observation_, reward, done, info = env.step(observation, real_action)
-            n_steps += 1
-            score += reward
-            agent.remember(observation, action, prob, val, reward, done)
-            if n_steps % N == 0:
-                agent.learn()
-                learn_iters += 1
-            observation = observation_
-        score_history.append(score)
-        avg_score = np.mean(score_history[-100:])
+    # check if the model is already trained and load it
+    try:
+        agent.load_models()
+        print('...loading saved models...')
+    except:
+        print('...no saved models, starting training from scratch...')
+        pass
 
-        if avg_score > best_score:
-            best_score = avg_score
-            agent.save_models()
+    observation = env.reset()
+    done = False
+    score = 0
+    while not done:
+        action, prob, val = agent.choose_action(observation)
+        real_action = env.action_space[action]
+        observation_, reward, done, info = env.step(observation, real_action)
+        n_steps += 1
+        score += reward
+        agent.remember(observation, action, prob, val, reward, done)
+        if n_steps % N == 0:
+            agent.learn()
+            learn_iters += 1
+        observation = observation_
+    score_history.append(score)
+    avg_score = np.mean(score_history[-100:])
 
-        print('episode', i, 'score %.1f' % score, 'avg score %.1f' % avg_score,
-                'time_steps', n_steps, 'learning_steps', learn_iters)
+    agent.save_models()
+
+    print('episode', i, 'score %.1f' % score, 'avg score %.1f' % avg_score,
+            'time_steps', n_steps, 'learning_steps', learn_iters)
+        
     x = [i+1 for i in range(len(score_history))]
     plot_learning_curve(x, score_history, figure_file)
