4f802cba919f1b776dea9b7c27d588752cb79672
Hashen Kottegoda <thkottegoda@gmail.com>
2025-07-19 23:06:16 +0200

    removed unused code

--- PPO-IMPL/utils.py
+++ PPO-IMPL/utils.py
@@ -2,9 +2,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def plot_learning_curve(x, scores, figure_file):
-    # running_avg = np.zeros(len(scores))
-    # for i in range(len(running_avg)):
-    #     running_avg[i] = np.mean(scores[max(0, i-100):(i+1)])
     plt.plot(x, scores)
     plt.title('Scores of each iteration')
     plt.savefig(figure_file)
